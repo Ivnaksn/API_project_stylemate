8890182cdd14338465876a2d826d165edb43a387
241Sultan <sltnamikomif23e@gmail.com>
2025-12-16 14:53:18 +0700

    feat: update API routes configuration

--- app/Config/Routes.php
+++ app/Config/Routes.php
@@ -13,11 +13,13 @@ $routes->group('api', function($routes) {
     $routes->post('login', 'AuthController::login');
     $routes->get('profile', 'AuthController::profile');
 
-    $routes->get('wardrobe', 'WardrobeController::index');
-    $routes->post('wardrobe', 'WardrobeController::create');
-    $routes->get('wardrobe/(:num)', 'WardrobeController::show/$1');
-    $routes->put('wardrobe/(:num)', 'WardrobeController::update/$1');
-    $routes->delete('wardrobe/(:num)', 'WardrobeController::delete/$1');
+   
+   $routes->get('wardrobe', 'WardrobeController::index ');
+   $routes->post('wardrobe', 'WardrobeController::create');
+   $routes->get('wardrobe/(:num)', 'WardrobeController::show/$1 ');
+   $routes->put('wardrobe/(:num)', 'WardrobeController::update/$1 ');
+   $routes->delete('wardrobe/(:num)', 'WardrobeController::delete/$1 ');
+    
 
 
 });
